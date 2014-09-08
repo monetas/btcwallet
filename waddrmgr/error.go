a7014f3e046e62ee66614ad09801999efe7bb45e
Lars Hesel Christensen <lars@monetas.net>
2014-09-08 16:27:33 +0200

    Add restrictions to replace and create series
    The minimum number of public keys is now three and the number of
    required signatures must be less than the number of public keys

--- waddrmgr/error.go
+++ waddrmgr/error.go
@@ -154,6 +154,8 @@ var errorCodeStrings = map[ErrorCode]string{
 	ErrKeysPrivatePublicMismatch: "ErrKeysPrivatePublicMismatch",
 	ErrVotingPoolNotExists:       "ErrVotingPoolNotExists",
 	ErrScriptCreation:            "ErrScriptCreation",
+	ErrTooFewPublicKeys:          "ErrTooFewPublicKeys",
+	ErrTooManyReqSignatures:      "ErrTooManyReqSignatures",
 }
 
 // String returns the ErrorCode as a human-readable name.
