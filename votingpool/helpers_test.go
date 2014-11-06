52dccd0719678f7856a728f359d357de6b61ebd8
Lars Hesel Christensen <lars@larshesel.dk>
2014-11-06 12:30:36 +0100

    Fix weird comment

--- votingpool/helpers_test.go
+++ votingpool/helpers_test.go
@@ -16,8 +16,8 @@ import (
 
 var activeNet = &btcnet.TestNet3Params
 
-// createInputs will create inputs a bunch if inputs to the address
-// defined by the pkScript.  See createInputsStore for a more flexible
+// createInputs will create a bunch of inputs for the address defined
+// by the pkScript.  See createInputsStore for a more flexible
 // version.
 func createInputs(t *testing.T, store *txstore.Store, pkScript []byte, amounts []int64) []txstore.Credit {
 	blockTxIndex := 1
