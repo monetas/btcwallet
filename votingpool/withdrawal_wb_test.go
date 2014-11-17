db99bf72ec17c733b18203b53bc8e6f96d5fc222
Guilherme Salgado <gsalgado@gmail.com>
2014-11-17 08:54:02 -0200

    Implement first pass check to determine if the requested outputs exceed the size of the eligible inputs
    Also adds a wb test to check tx outputs match the requested ones.

--- votingpool/withdrawal_wb_test.go
+++ votingpool/withdrawal_wb_test.go
@@ -17,8 +17,12 @@
 package votingpool
 
 import (
+	"sort"
 	"testing"
 
+	"github.com/conformal/btcnet"
+	"github.com/conformal/btcscript"
+	"github.com/conformal/btcutil"
 	"github.com/conformal/btcutil/hdkeychain"
 	"github.com/conformal/btcwallet/waddrmgr"
 	"github.com/conformal/btcwire"
@@ -49,8 +53,131 @@ func TestTxInSigning(t *testing.T) {
 	signTxAndValidate(t, mgr, tx, sigs[ntxid], credits)
 }
 
-func TestWithdrawalOutputs(t *testing.T) {
-	// TODO: Check that the outputs in the constructed TX match the requested ones.
+// Check that all outputs requested in a withdrawal match the outputs of the generated
+// transaction(s).
+func TestWithdrawalTxOutputs(t *testing.T) {
+	teardown, mgr, pool := TstSetUp(t)
+	store, storeTearDown := TstCreateTxStore(t)
+	defer teardown()
+	defer storeTearDown()
+
+	// Create eligible inputs and the list of outputs we need to fulfil.
+	eligible := TstCreateCredits(t, pool, []int64{2e6, 4e6}, store)
+	outputs := []*OutputRequest{
+		NewOutputRequest("foo", 1, "34eVkREKgvvGASZW7hkgE2uNc1yycntMK6", btcutil.Amount(3e6)),
+		NewOutputRequest("foo", 2, "3PbExiaztsSYgh6zeMswC49hLUwhTQ86XG", btcutil.Amount(2e6)),
+	}
+	changeStart, err := pool.ChangeAddress(0, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := newWithdrawal(0, outputs, eligible, changeStart, mgr.Net())
+	if err := w.fulfilOutputs(); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(w.transactions) != 1 {
+		t.Fatalf("Unexpected number of transactions; got %d, want 1", len(w.transactions))
+	}
+
+	tx := w.transactions[0]
+	// The created tx should include both eligible credits, so we expect it to have
+	// an input amount of 2e6+4e6 satoshis.
+	inputAmount := eligible[0].Amount() + eligible[1].Amount()
+	change := inputAmount - (outputs[0].amount + outputs[1].amount + calculateFee(tx))
+	expectedOutputs := append(
+		outputs, NewOutputRequest("foo", 3, changeStart.Addr().String(), change))
+	checkTxOutputs(t, tx, expectedOutputs, mgr.Net())
+}
+
+// Check that withdrawal.status correctly states that no outputs were fulfilled when we
+// don't have enough eligible credits for any of them.
+func TestFulfilOutputsNoSatisfiableOutputs(t *testing.T) {
+	teardown, mgr, pool := TstSetUp(t)
+	store, storeTearDown := TstCreateTxStore(t)
+	defer teardown()
+	defer storeTearDown()
+
+	eligible := TstCreateCredits(t, pool, []int64{1e6}, store)
+	outputs := []*OutputRequest{
+		NewOutputRequest("foo", 1, "3Qt1EaKRD9g9FeL2DGkLLswhK1AKmmXFSe", btcutil.Amount(3e6))}
+	changeStart, err := pool.ChangeAddress(0, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := newWithdrawal(0, outputs, eligible, changeStart, mgr.Net())
+	if err := w.fulfilOutputs(); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(w.transactions) != 0 {
+		t.Fatalf("Unexpected number of transactions; got %d, want 0", len(w.transactions))
+	}
+
+	if len(w.status.outputs) != 1 {
+		t.Fatalf("Unexpected number of outputs in WithdrawalStatus; got %d, want 1",
+			len(w.status.outputs))
+	}
+
+	if w.status.outputs[0].status != "partial-" {
+		t.Fatalf("Unexpected status for requested outputs; got '%s', want 'partial-'",
+			w.status.outputs[0].status)
+	}
+}
+
+// Check that some requested outputs are not fulfilled when we don't have credits for all
+// of them.
+func TestFulfilOutputsNotEnoughCreditsForAllRequests(t *testing.T) {
+	teardown, mgr, pool := TstSetUp(t)
+	store, storeTearDown := TstCreateTxStore(t)
+	defer teardown()
+	defer storeTearDown()
+
+	// Create eligible inputs and the list of outputs we need to fulfil.
+	eligible := TstCreateCredits(t, pool, []int64{2e6, 4e6}, store)
+	out1 := NewOutputRequest("foo", 1, "34eVkREKgvvGASZW7hkgE2uNc1yycntMK6", btcutil.Amount(3e6))
+	out2 := NewOutputRequest("foo", 2, "3PbExiaztsSYgh6zeMswC49hLUwhTQ86XG", btcutil.Amount(2e6))
+	out3 := NewOutputRequest("foo", 3, "3Qt1EaKRD9g9FeL2DGkLLswhK1AKmmXFSe", btcutil.Amount(5e6))
+	outputs := []*OutputRequest{out1, out2, out3}
+	changeStart, err := pool.ChangeAddress(0, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := newWithdrawal(0, outputs, eligible, changeStart, mgr.Net())
+	if err := w.fulfilOutputs(); err != nil {
+		t.Fatal(err)
+	}
+
+	tx := w.transactions[0]
+	// The created tx should spend both eligible credits, so we expect it to have
+	// an input amount of 2e6+4e6 satoshis.
+	inputAmount := eligible[0].Amount() + eligible[1].Amount()
+	// We expect it to include outputs for requests 1 and 2, plus a change output, but
+	// output request #3 should not be there because we don't have enough credits.
+	change := inputAmount - (out1.amount + out2.amount + calculateFee(tx))
+	expectedOutputs := []*OutputRequest{out1, out2}
+	sort.Sort(byOutBailmentID(expectedOutputs))
+	expectedOutputs = append(
+		expectedOutputs, NewOutputRequest("foo", 4, changeStart.Addr().String(), change))
+	checkTxOutputs(t, tx, expectedOutputs, mgr.Net())
+
+	// withdrawal.status should state that outputs 1 and 2 were successfully fulfilled,
+	// and that output 3 was not.
+	expectedStatuses := map[*OutputRequest]string{
+		out1: "success", out2: "success", out3: "partial-"}
+	for _, wOutput := range w.status.outputs {
+		if wOutput.status != expectedStatuses[wOutput.request] {
+			t.Fatalf("Unexpected status for %v; got '%s', want 'partial-'", wOutput.request,
+				wOutput.status)
+		}
+	}
+}
+
+func TestWithdrawalOutputsNoChange(t *testing.T) {
+	// TODO:
 }
 
 func TestSignMultiSigUTXOInvalidPkScript(t *testing.T) {
@@ -114,4 +241,39 @@ func countNonNil(keys []*hdkeychain.ExtendedKey) int {
 		}
 	}
 	return count
+}
+
+// pkScriptAddr parses the given pkScript and returns the address associated with it.
+func pkScriptAddr(t *testing.T, pkScript []byte, net *btcnet.Params) string {
+	_, addresses, _, err := btcscript.ExtractPkScriptAddrs(pkScript, net)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(addresses) != 1 {
+		t.Fatalf("Unexpected number of addresses in pkScript; got %d, want 1", len(addresses))
+	}
+	return addresses[0].String()
+}
+
+// checkTxOutputs checks that the address and amount of every output in the given tx match the
+// address and amount of every item in the slice of OutputRequests.
+func checkTxOutputs(t *testing.T, tx *btcwire.MsgTx, expectedOutputs []*OutputRequest, net *btcnet.Params) {
+	nOutputs := len(expectedOutputs)
+	if len(tx.TxOut) != nOutputs {
+		t.Fatalf("Unexpected number of tx outputs; got %d, want %d", len(tx.TxOut), nOutputs)
+	}
+
+	for i, output := range expectedOutputs {
+		txOut := tx.TxOut[i]
+		gotAddr := pkScriptAddr(t, txOut.PkScript, net)
+		if gotAddr != output.address {
+			t.Fatalf(
+				"Unexpected address for output %d; got %s, want %s", i, gotAddr, output.address)
+		}
+		gotAmount := btcutil.Amount(txOut.Value)
+		if gotAmount != output.amount {
+			t.Fatalf(
+				"Unexpected amount for output %d; got %v, want %v", i, gotAmount, output.amount)
+		}
+	}
 }
